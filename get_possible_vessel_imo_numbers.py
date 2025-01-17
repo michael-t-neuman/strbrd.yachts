89620c761bed9abb5009200666676f3025ad586a
michael-t-neuman <michaeltneuman+github@gmail.com>
2025-01-17 12:10:28 -0600

    Update get_possible_vessel_imo_numbers.py

--- get_possible_vessel_imo_numbers.py
+++ get_possible_vessel_imo_numbers.py
@@ -29,13 +29,22 @@ def write_to_csv(result_queue,imo_queue):
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(funcName)s - %(message)s')
     logging.getLogger().setLevel(logging.INFO)
-    random_range = list(range(9999999))
+    random_range = set(range(9999999))
+    with open('imos.csv','r',encoding='utf-8') as infile:
+        already_seen = set()
+        for imo_number_already_found in infile.read().splitlines():
+            if imo_number_already_found in already_seen:
+                continue
+            already_seen.add(imo_number_already_found)
+            random_range.remove(int(imo_number_already_found))
+    random_range = list(random_range)
     random.shuffle(random_range)
     imo_queue = queue.Queue()
     result_queue = queue.Queue()
     for imo in random_range:
         imo_queue.put(imo)
     writer_thread = threading.Thread(target=write_to_csv, args=(result_queue,imo_queue,num_boats_found_so_far))
+    writer_thread.daemon = True
     writer_thread.start()
     for _ in range(3):
         thread = threading.Thread(target=fetch_imo, args=(imo_queue, result_queue))
